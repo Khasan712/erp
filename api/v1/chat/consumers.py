ecdf467d7ae3c6049c38fc0fed75c3118128b546
Khasan <info.kamalov@gmail.com>
2023-03-23 00:50:27 +0500

    ..

--- api/v1/chat/consumers.py
+++ api/v1/chat/consumers.py
@@ -1,100 +1,50 @@
 from channels.generic.websocket import JsonWebsocketConsumer, WebsocketConsumer
 from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
 from api.v1.chat.models import ChatRoom, Chat
 from api.v1.users.models import User
+from asgiref.sync import async_to_sync
+import json
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    def get_user(self, pk):
-        return User.objects.get(id=pk)
-
-    # def get_online_users(self):
-    #     online_users = OnlineUser.objects.all()
-    #     return [onlineUser.user.id for onlineUser in onlineUsers]
-
-    # def addOnlineUser(self, user):
-    #     try:
-    #         OnlineUser.objects.create(user=user)
-    #     except:
-    #         pass
-
-    # def deleteOnlineUser(self, user):
-    # 	try:
-    # 		OnlineUser.objects.get(user=user).delete()
-    # 	except:
-    # 		pass
-
-    def save_message(self, message, pk, room_id):
-        user_obj = User.objects.get(id=pk)
-        chat_room_obj = ChatRoom.objects.get(room_id=room_id)
-        chat_obj = Chat.objects.create(
-            chat=chat_room_obj, user_id=user_obj.id, message=message
+class TextRoomConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = self.room_name
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
         )
-        return {
-            'action': 'message',
-            'user': user_obj,
-            'roomId': room_id,
-            'message': message,
-        }
+        self.accept()
 
-    # async def sendOnlineUserList(self):
-    #     onlineUserList = await database_sync_to_async(self.getOnlineUsers)()
-    #     chatMessage = {
-    #         'type': 'chat_message',
-    #         'message': {
-    #             'action': 'onlineUser',
-    #             'userList': onlineUserList
-    #         }
-    #     }
-    #     await self.channel_layer.group_send('onlineUser', chatMessage)
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    async def connect(self):
-        self.userId = self.scope['url_route']['kwargs']['userId']
-        self.userRooms = await database_sync_to_async(
-            list
-        )(ChatRoom.objects.filter(member=self.userId))
-        for room in self.userRooms:
-            await self.channel_layer.group_add(
-                room.roomId,
-                self.channel_name
-            )
-        await self.channel_layer.group_add('onlineUser', self.channel_name)
-        self.user = await database_sync_to_async(self.getUser)(self.userId)
-        await database_sync_to_async(self.addOnlineUser)(self.user)
-        await self.sendOnlineUserList()
-        await self.accept()
+    def receive(self, text_data):
+        # Receive message from WebSocket
+        text_data_json = json.loads(text_data)
+        text = text_data_json['text']
+        sender = text_data_json['sender']
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': text,
+                'sender': sender
+            }
+        )
 
-    # async def disconnect(self, close_code):
-    #     await database_sync_to_async(self.deleteOnlineUser)(self.user)
-    #     await self.sendOnlineUserList()
-    #     for room in self.userRooms:
-    #         await self.channel_layer.group_discard(
-    #             room.roomId,
-    #             self.channel_name
-    #         )
-    #
-    # async def receive(self, text_data):
-    # 	text_data_json = json.loads(text_data)
-    # 	action = text_data_json['action']
-    # 	roomId = text_data_json['roomId']
-    # 	chatMessage = {}
-    # 	if action == 'message':
-    # 		message = text_data_json['message']
-    # 		userId = text_data_json['user']
-    # 		chatMessage = await database_sync_to_async(
-    # 			self.saveMessage
-    # 		)(message, userId, roomId)
-    # 	elif action == 'typing':
-    # 		chatMessage = text_data_json
-    # 	await self.channel_layer.group_send(
-    # 		roomId,
-    # 		{
-    # 			'type': 'chat_message',
-    # 			'message': chatMessage
-    # 		}
-    # 	)
-    #
-    # async def chat_message(self, event):
-    # 	message = event['message']
-    # 	await self.send(text_data=json.dumps(message))
+    def chat_message(self, event):
+        # Receive message from room group
+        text = event['message']
+        sender = event['sender']
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'text': text,
+            'sender': sender
+        }))
--- config/routing.py
+++ config/routing.py
@@ -1,7 +1,20 @@
 from django.urls import path
 
-from api.v1.chat.consumers import ChatConsumer
+# from api.v1.chat.consumers import ChatConsumer
 
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+# import app.routing
+from django.urls import re_path
+from api.v1.chat.consumers import TextRoomConsumer
 websocket_urlpatterns = [
-    path("", ChatConsumer.as_asgi()),
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
 ]
+# the websocket will open at 127.0.0.1:8000/ws/<room_name>
+application = ProtocolTypeRouter({
+    'websocket':
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ,
+})
